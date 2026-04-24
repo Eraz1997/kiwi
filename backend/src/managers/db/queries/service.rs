c16d98e95355e541e2abbc8274f93da4b438b5ad
Eraz1997 <eraz1997@live.it>
2026-04-25 00:33:09 +0200

    fix: service query

--- backend/src/managers/db/queries/service.rs
+++ backend/src/managers/db/queries/service.rs
@@ -66,7 +66,7 @@ impl DbManager {
                 github_repository,
                 required_role
             ) VALUES (
-                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
+                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
             ) RETURNING
                 name,
                 image_name,
